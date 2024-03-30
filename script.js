cdde393679eecc7a6de60efe44e3e90656322a61
AnkitXGandhi <agproxy21@yahoo.com>
2024-03-30 21:40:16 +0530

    update 1.2

--- script.js
+++ script.js
@@ -11,14 +11,20 @@ buttonsArray.forEach(btn =>{
             string = string.substring(0,string.length-1);
             input.value=string;
         }
+        
         else if(e.target.innerHTML=='AC'){
             string='';
             input.value=string;
         }
         else if(e.target.innerHTML=='='){
-            string=eval(string);
-            input.value=string;
+            
+          
+                string=eval(string);
+                input.value=string;
+            
+            
         }
+        
         else{
             string += e.target.innerHTML;
             input.value = string; 
