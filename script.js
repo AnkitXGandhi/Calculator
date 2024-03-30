dc78beb795c7cb5abc8e0fa1bec8c39e2c1e6a01
AnkitXGandhi <agproxy21@yahoo.com>
2024-03-30 13:34:38 +0530

    final

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ let string ='';
 
 buttonsArray.forEach(btn =>{
     btn.addEventListener('click',  (e) => {
-        string = e.target.innerHTML;
-        input.value = string;
-        console.log(e.target.innerHTML);
+        
+        if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else if(e.target.innerHTML=='AC'){
+            string='';
+            input.value=string;
+        }
+        else if(e.target.innerHTML=='='){
+            string=eval(string);
+            input.value=string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string; 
+        }
+        
+       
     });
 });
